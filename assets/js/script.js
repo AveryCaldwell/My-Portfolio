f480c6eae31e6479466b4a4a8a7b4161d20d976d
Avery <averycaldwell7@gmail.com>
2023-01-28 14:22:52 -0800

    edit button, bckground and styling

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+let myBtn = document.getElementById('myBtn');
+let coverpage = document.getElementById('coverpage');
+let container = document.getElementById('container');
+
+// Hides sections until active
+const hideSections = function () {
+  coverpage.style.display = 'none';
+  container.style.display = 'none';
+};
+
+// Dsiplays active sections by id's
+function activeSection(section) {
+  hideSections();
+  if (section === 'coverpage') {
+    coverpage.style.display = 'block';
+  } else if (section === 'content') {
+    content.style.display = 'block';
+  } else if (section === 'container') {
+    container.style.display = 'block';
+  }
+}
+
+myBtn.addEventListener('click', function () {
+  currentPage = 1;
+  activeSection('container');
+});
+// Tells which page we are currently on
+let currentPage = 1;
+
+// Load coverpage and hide container on load
+window.addEventListener('load', (event) => {
+  hideSections('container');
+  activeSection('coverpage');
+});
