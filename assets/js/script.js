760f25ecf4410fbcea5c554bbf79e3d57e996ca3
Avery <averycaldwell7@gmail.com>
2023-01-30 18:22:56 -0800

    hiding coverpage for review

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,31 +4,31 @@ let container = document.getElementById('container');
 
 // Hides sections until active
 const hideSections = function () {
-  coverpage.style.display = 'none';
-  container.style.display = 'none';
+    coverpage.style.display = 'none';
+    // container.style.display = 'none';
 };
 
 // Dsiplays active sections by id's
-function activeSection(section) {
-  hideSections();
-  if (section === 'coverpage') {
-    coverpage.style.display = 'block';
-  } else if (section === 'content') {
-    content.style.display = 'block';
-  } else if (section === 'container') {
-    container.style.display = 'block';
-  }
-}
+// function activeSection(section) {
+// hideSections();
+// if (section === 'coverpage') {
+// coverpage.style.display = 'block';
+// } else if (section === 'content') {
+// content.style.display = 'block';
+// } else if (section === 'container') {
+// container.style.display = 'block';
+// }
+// }
 
 myBtn.addEventListener('click', function () {
-  currentPage = 1;
-  activeSection('container');
+    currentPage = 1;
+    activeSection('container');
 });
 // Tells which page we are currently on
 let currentPage = 1;
 
 // Load coverpage and hide container on load
 window.addEventListener('load', (event) => {
-  hideSections('container');
-  activeSection('coverpage');
+    hideSections('container');
+    activeSection('coverpage');
 });
